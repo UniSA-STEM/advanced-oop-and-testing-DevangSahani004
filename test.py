1355b476047e890e4dd3500a9ccbbcbd12c52287
Devang Sahani <133926800+DevangSahani004@users.noreply.github.com>
2025-12-03 00:35:41 +1030

    using pytest, added health restriction test for animals under treatment. Had issue with being able to determine whether animal could be moved or not.

--- test.py
+++ test.py
@@ -15,3 +15,9 @@ def test_add_animal_success(lion, savannah):
     result = savannah.add_animal(lion)
     assert "Leo added" in result
     assert lion in savannah.get_animals()
+
+def test_health_restriction(lion, savannah):
+    lion.add_health_record("Broken paw", "2025-12-02", "High", "Bandaged")
+    result = savannah.add_animal(lion)
+    assert "cannot be moved" in result
+    assert lion not in savannah.get_animals()
