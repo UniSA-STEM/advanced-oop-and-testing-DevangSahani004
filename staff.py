74eced06d32f1293160a5ceb1c813e45e55efe3c
Devang Sahani <133926800+DevangSahani004@users.noreply.github.com>
2025-12-02 23:49:38 +1030

    developed class structure for staff. Setup required getter methods to retrieve information from private attributes. Also developed method to assign a staff member to enclosure.

--- staff.py
+++ staff.py
@@ -1,8 +1,28 @@
 '''
-File: filename.py
+File: staff.py
 Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+Author: Devang Sahani
+ID: 110411585
+Username: sahdy004
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+class Staff:
+    """represents a generic staff member in the zoo"""
+    def __init__(self, name, role):
+        self.__name = name   # staff member's name
+        self.__role = role   # role type like zookeeper or veterinarian
+        self.__assigned = None   # track which enclosure they are assigned to
+
+    def get_name(self):
+        return self.__name   # getter method to return the staff member's name
+
+    def get_role(self):
+        return self.__role   # getter method to return the staff member's role
+
+    def assign_to_enclosure(self, enclosure):
+        self.__assigned = enclosure   # assign staff to a specific enclosure
+        return f"{self.get_name()} is now assigned to {enclosure.get_name()}."   # confirmation message
+
+    def get_assignment(self):
+        return self.__assigned   # getter method to return the enclosure details the staff is assigned to
